9a4200c06828a4f670ae55573ad3ac5039285c7a
Carlos Alessandro <carllosalessandro@gmail.com>
2024-03-14 16:13:32 -0300

    feat: add navbar and title

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
     <Navbar />
+    <h1 className="text-center text-6xl text-cyan-950 font-bold p-2">Project React</h1>
       <div className="flex mx-auto w-11/12">
         <AvailableProducts />
         <Cart />
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,13 +1,13 @@
 export default function Navbar() {
   return (
-    <nav className="w-full border-b">
-      <main className="w-11/12">
+    <nav className="w-full border-b p-3 shadow-md sticky mb-5">
+      <main className="w-11/12 mx-auto">
         <ul className="flex justify-around">
-          <li>React</li>
-          <li>Zustand</li>
-          <li>Tailwind</li>
-          <li>Faker</li>
-          <li>Vite</li>
+          <li className="text-lg text-blue-800 font-semibold">React</li>
+          <li className="text-lg text-orange-900 font-semibold">Zustand</li>
+          <li className="text-lg text-cyan-400 font-semibold">Tailwind</li>
+          <li className="text-lg text-gray-600 font-semibold">Faker</li>
+          <li className="text-lg text-yellow-400 font-semibold">Vite</li>
         </ul>
       </main>
     </nav>
